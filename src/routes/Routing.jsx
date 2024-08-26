879ada13064eb295d9f77ad382f57bcf1b6bc9bc
Deepak Dhiman <dhimandeepak957@gmail.com>
2024-08-26 23:00:55 +0530

    tearms and condition

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 // pages
 const Home = lazy(() => import("../pages/home/Home"));
 const PrivacyPolicy = lazy(() => import("../pages/policy/PrivacyPolicy"));
+const TearmsCondition = lazy(() => import("../pages/policy/TearmsCondition"));
 
 const Routing = () => {
     return <>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/tearms-and-condition" element={<TearmsCondition />} />
         </Routes>
     </>
 }
